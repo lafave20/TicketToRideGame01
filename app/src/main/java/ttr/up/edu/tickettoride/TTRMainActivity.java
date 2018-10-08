667acbd9bee659bd1a7c06b9f93013214f68ea1d
Lexden12 <lexden.s@gmail.com>
2018-10-08 09:14:02 -0700

    Add New packages and files for the game framework. Start work on the GameState for TTR.

---
+++ app/src/main/java/ttr/up/edu/tickettoride/TTRMainActivity.java
@@ -0,0 +1,13 @@
+package ttr.up.edu.tickettoride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TTRMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/ttr/up/edu/tickettoride/TTR_GameState.java
@@ -0,0 +1,13 @@
+package ttr.up.edu.tickettoride;
+
+import java.util.ArrayList;
+
+public class TTR_GameState {
+    private TrainDeck trainDeck;
+    private RouteDeck routeDeck;
+    private ArrayList<edu.up.cs301.game.GamePlayer> players;
+    private TrainStash trainStash;
+    private Board board;
+    private int currentPlayer;
+    private Routes routes;
+}
